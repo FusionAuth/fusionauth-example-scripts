f6f2ade58b96dbc1c5057cf74977122aac6006d0
Dan Moore <dan@fusionauth.io>
2023-09-15 05:40:55 -0600

    support username as well as email address

--- full-user-search/fulluserdownload.sh
+++ full-user-search/fulluserdownload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-API_KEY=...
+API_KEY=this_really_should_be_a_long_random_alphanumeric_value_but_this_still_works
 FA_HOSTNAME=http://localhost:9011
 
 max_total=0
@@ -8,9 +8,10 @@ max_total=0
 # ES is not case sensitive
 
 for fc in {a..z} {0..9}; do
-    #echo "$f"
+    #echo "$fc"
     printf "."
-    total=`curl -s -H "Authorization: $API_KEY" $FA_HOSTNAME'/api/user/search/?queryString=email:'$fc'*&accurateTotal=true' | jq '.total'`
+    total=`curl -s -H "Authorization: $API_KEY" $FA_HOSTNAME'/api/user/search/?queryString=email:'$fc'*%20OR%20username:'$fc'*&accurateTotal=true' | jq '.total'`
+    #echo $total
     if [ $total -gt $max_total ]; then
       max_total=$total
     fi
@@ -23,14 +24,14 @@ if [ $max_total -lt 10000 ]; then
   # all of our segments are less than 10k
   for fc in {a..z} {0..9}; do
     printf "."
-    curl -s -H "Authorization: $API_KEY" $FA_HOSTNAME'/api/user/search/?queryString=email:'$fc'*&numberOfResults=10000' > $tmpdir/file.$fc
+    curl -s -H "Authorization: $API_KEY" $FA_HOSTNAME'/api/user/search/?queryString=email:'$fc'*%20OR%20username:'$fc'*&numberOfResults=10000' > $tmpdir/file.$fc
   done
 else 
   # we handle two characters here, but you could do the same count check and go deeper
   for fc in {a..z} {0..9}; do
     for sc in {a..z} {0..9}; do
       printf "."
-      curl -s -H "Authorization: $API_KEY" $FA_HOSTNAME'/api/user/search/?queryString=email:'$fc$sc'*&numberOfResults=10000' > $tmpdir/file.$fc.$sc
+      curl -s -H "Authorization: $API_KEY" $FA_HOSTNAME'/api/user/search/?queryString=email:'$fc$sc'*%20OR%20username:'$fc$sc'*&numberOfResults=10000' > $tmpdir/file.$fc.$sc
     done
   done
 fi
